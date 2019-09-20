f3861abec948ab1784588d77b02e7db1c8e7c15d
ongraphpythondev <pythondev@ongraph.com>
2019-09-20 12:15:10 +0530

    add render in login view

--- PhoneLogin/verify_app/views.py
+++ PhoneLogin/verify_app/views.py
@@ -16,7 +16,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request,user)
-                    return HttpResponse('Authenticated successfully')
+                    return render(request, 'verify_app/login_done.html')
                 else:
                     return HttpResponse('Disabled account')
             else:
